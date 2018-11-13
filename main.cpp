116064506df6e26852cae34daa47afe5a6e50293
G-Houlmann <gildas.houlmann@gmail.com>
2018-11-13 14:35:06 +0100

    structure de base
    constantes, variables et boucles principales

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : <XX>
+ Laboratoire : 03
  Fichier     : main.cpp
  Auteur(s)   : Thomas Ricci
  Date        : 26.09.2018
@@ -24,9 +24,9 @@ int main() {
    const unsigned int NBR_EXP_MIN = 1000;
    const unsigned int NBR_EXP_MAX = 100000;
    
-   const unsigned int COTE_MAX = 50;
-   const unsigned int COTE_MIN = 2;
-   const unsigned int COTE_INCREMENT = 2;
+   const unsigned int TAILLE_GRILLE_MAX = 50;
+   const unsigned int TAILLE_GRILLE_MIN = 2;
+   const unsigned int TAILLE_GRILLE_INCREMENT = 2;
    
    const unsigned short DIRECTION_DROITE = 1;
    const unsigned short DIRECTION_GAUCHE = 2;
@@ -38,6 +38,7 @@ int main() {
    unsigned int x,y;
    unsigned int compteur, nbrExperience;
    
+   //Demander à l'utilisateur d'entrer le nombre d'expériences à effectuer
    do{
       cout << "Veuillez entrer un nombre d'experience " 
            << "( entre " << NBR_EXP_MIN << " et " << NBR_EXP_MAX << "): ";
@@ -47,6 +48,13 @@ int main() {
          cin.ignore(INT_MAX,'\n');
       }
    }while(nbrExperience <NBR_EXP_MIN || nbrExperience > NBR_EXP_MAX);
+   
+   //Effectuer les expériences
+   for(int i = TAILLE_GRILLE_MIN; i < TAILLE_GRILLE_MAX; i += TAILLE_GRILLE_INCREMENT){
+      for(int j = 0; j < nbrExperience; j ++){
+         
+      }
+   }
    
    
    return EXIT_SUCCESS;
